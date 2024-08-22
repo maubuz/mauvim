235f5631387dbbd08797d4d7b9fc72184ea168ef
Mauricio Buschinelli <mauricio.buschinelli@canonical.com>
2024-08-22 16:47:43 -0400

    add bash LSP server as dependency

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -170,6 +170,7 @@ return {
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        bashls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -208,6 +209,9 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'shfmt',
+        'shellcheck',
+        -- 'bash-debug-adaptor',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
