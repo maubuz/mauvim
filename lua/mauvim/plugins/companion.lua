9d6a74d1a51df7026fbedf3d93bd366dc0f7d9cf
Mauricio Buschinelli <mauricio.buschinelli@canonical.com>
2025-01-13 21:41:59 -0500

    add companion AI plugin

--- lua/mauvim/plugins/companion.lua
+++ lua/mauvim/plugins/companion.lua
@@ -5,14 +5,23 @@ return {
     'nvim-treesitter/nvim-treesitter',
     { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } },
   },
-  config = {
-    strategies = {
-      chat = {
-        adapter = 'copilot',
-      },
-      inline = {
-        adapter = 'copilot',
-      },
+  strategies = {
+    chat = {
+      adapter = 'copilot',
+    },
+    inline = {
+      adapter = 'copilot',
     },
   },
+  opts = {},
+  keys = {
+    { '<leader>a', '<cmd>CodeCompanionActions<cr>', mode = 'n', desc = 'CodeCompanion Actions', noremap = true, silent = true },
+    { '<leader>a', '<cmd>CodeCompanionActions<cr>', mode = 'v', desc = 'CodeCompanion Actions', noremap = true, silent = true },
+    { '<leader>tc', '<cmd>CodeCompanionChat Toggle<cr>', mode = 'n', desc = 'CodeCompanion Chat', noremap = true, silent = true },
+    { '<leader>tc', '<cmd>CodeCompanionChat Toggle<cr>', mode = 'v', desc = 'CodeCompanion Chat', noremap = true, silent = true },
+    { '<leader>ad', '<cmd>CodeCompanionChat Add<cr>', mode = 'v', desc = 'CodeCompanion Add Chat', noremap = true, silent = true },
+  },
+  init = function()
+    vim.cmd [[cab cc CodeCompanion]]
+  end,
 }
