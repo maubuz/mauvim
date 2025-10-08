1f2cc4bfd4f3f0890aa474271917be439dadb607
Mauricio Buschinelli <mauri.andres@gmail.com>
2025-10-07 21:48:42 -0400

    add toggle for indent blank line

--- lua/mauvim/plugins/indent.lua
+++ lua/mauvim/plugins/indent.lua
@@ -1,9 +1,12 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {},
+  keys = {
+    { '<leader>ti', '<cmd>IBLToggle<cr>', mode = 'n', desc = 'Toggle indent blankline', noremap = true, silent = true },
+  },
 }
 
 -- To toggle, use the built-in comman:
